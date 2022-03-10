acfc5eecd6c23ba4f5f6ea2f1a716d009bb1c66f
07983REYJ <46697301+07983REYJ@users.noreply.github.com>
2022-03-09 23:46:02 -0500

    Revised case statement

--- calculator.rb
+++ calculator.rb
@@ -6,17 +6,29 @@ def integer?(input)
   input.to_i.to_s == input
 end
 
-def operation_to_message(op)
-  case op
-  when "1"
-    "Adding"
-  when "2"
-    "Subtracting"
-  when "3"
-    "Multiplying"
-  when "4"
-    "Dividing"
-  end
+def float?(input)
+  input.to_f.to_s == input
+end
+
+def number?(input)
+  integer?(input) || float?(input)
+end
+
+def operation_to_message(operation)
+  word = case operation
+    when "1"
+      "Adding"
+    when "2"
+      "Subtracting"
+    when "3"
+      "Multiplying"
+    when "4"
+      "Dividing"
+    end
+
+  x = "A random line of code"
+
+  word
 end
 
 prompt("Welcome to Calculator! Enter your name:")
@@ -36,11 +48,12 @@ prompt("Hello. Welcome #{name}")
 
 loop do
   number_1 = ""
+
   loop do
     prompt("Enter a number: ")
     number_1 = Kernel.gets().chomp()
 
-    if integer?(number_1)
+    if number?(number_1)
       break
     else
       prompt("Hmm... that doesn't look like a valid number")
@@ -52,7 +65,7 @@ loop do
     prompt("Enter another number: ")
     number_2 = Kernel.gets().chomp()
 
-    if integer?(number_2)
+    if number?(number_2)
       break
     else
       prompt("Sorry, that is not a valid number.")
@@ -86,13 +99,13 @@ Choose an operation:
 
   result = case operator
     when "1"
-      number_1.to_i() + number_2.to_i()
+      number_1.to_f() + number_2.to_f()
     when "2"
-      number_1.to_i() - number_2.to_i()
+      number_1.to_f() - number_2.to_f()
     when "3"
-      number_1.to_i() * number_2.to_i()
+      number_1.to_f() * number_2.to_f()
     when "4"
-      number_1.to_i() / number_2.to_f()
+      number_1.to_f() / number_2.to_f()
     end
 
   prompt("The result is #{result}")
