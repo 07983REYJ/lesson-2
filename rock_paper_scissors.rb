7d849694e6b0a139f0b70bef06721d30752b45b9
07983REYJ <46697301+07983REYJ@users.noreply.github.com>
2022-03-16 23:50:04 -0400

    added win/loss scenarios

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -19,3 +19,15 @@ end
 computer_choice = VALID_CHOICES.sample
 
 Kernel.puts("You chose: #{choice}  ;  Computer chose: #{computer_choice}")
+
+if (choice == "rock" && computer_choice == "scissors") ||
+   (choice == "paper" && computer_choice == "rock") ||
+   (choice == "scissors" && computer_choice == "paper")
+  prompt("You won!")
+elsif (choice == "rock" && computer_choice == "paper") ||
+      (choice == "paper" && computer_choice == "scissors") ||
+      (choice == "scissors" && computer_choice == "rock")
+  prompt("Computer won!")
+else
+  prompt("It's a tie!")
+end
